f08716fae1deef8fb45cb34ece193bc9f5bffcff
Konstantin Ilchenko <konstantin.ilchenko@altoros.com>
2013-10-30 21:24:38 +0300

    Add publish settings

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,3 +5,4 @@ data_bag_path "data_bags"
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
+knife[:azure_publish_settings_file] = "/home/test/bosh-workspace/azure/simpl1g.publishsettings"
